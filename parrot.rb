afc71349174d82d6004359caf30988129fcbd573
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-08-05 18:19:21 +1000

    commit tree 2b894222adfcfdab982836b900d8d1535fbd37a0

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,20 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase)
+  
+    if phrase ==""
+       puts "Squawk!"
+       return "Squawk!"
+   
+    elsif phrase == 'Pretty bird!'
+       puts "#{phrase}"
+       return "#{phrase}"
+
+    end
+
+end
+parrot()
+
+
+
+
